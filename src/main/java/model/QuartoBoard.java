a4b90ae9f79b41afc19c9670e4b593e6f975c2db
Spencer Thomas <srt4@uw.edu>
2017-05-30 20:10:19 -0700

    Seed board with all available pieces

--- src/main/java/model/QuartoBoard.java
+++ src/main/java/model/QuartoBoard.java
@@ -1,9 +1,15 @@
 package model;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import model.attribute.Fill;
+import model.attribute.Height;
+import model.attribute.Hue;
+import model.attribute.Shape;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -17,7 +23,7 @@ public class QuartoBoard {
     private final QuartoPiece[][] board;
 
     public QuartoBoard() {
-        this.availablePieces = Sets.newHashSet();
+        this.availablePieces = Sets.newHashSet(createAllPieces());
         this.board = new QuartoPiece[4][4];
     }
 
@@ -37,9 +43,23 @@ public class QuartoBoard {
         if (board[x][y] != null) {
             throw new IllegalArgumentException("Piece cannot be played as space is occupied. Occupant=" + board[x][y]);
         }
-        
+
         board[x][y] = piece;
         availablePieces.remove(piece);
+    }
+
+    private List<QuartoPiece> createAllPieces() {
+        final List<QuartoPiece> pieces = Lists.newLinkedList();
+        for (final Fill fill : Fill.values()) {
+            for (final Height height : Height.values()) {
+                for (final Hue hue : Hue.values()) {
+                    for (final Shape shape : Shape.values()) {
+                        pieces.add(PieceFactory.createPiece(shape, fill, height, hue));
+                    }
+                }
+            }
+        }
+        return pieces;
     }
 
 }
