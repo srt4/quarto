793e163933f5fccf942f9a997248634ddcf7f184
Brent Generous <brentgenerous@gmail.com>
2017-06-01 23:11:16 -0700

    WIP: Main game loop

--- src/main/java/model/QuartoGame.java
+++ src/main/java/model/QuartoGame.java
@@ -4,13 +4,14 @@ public class QuartoGame {
     
     private final QuartoBoard board;
     private final QuartoPieces availablePieces;
-    // TODO: Implement player class
-    // public final Player p1;
-    // public final Player p2;
+    public final Player p1;
+    public final Player p2;
     
     public QuartoGame() {
         this.board = new QuartoBoard();
         this.availablePieces = new QuartoPieces();
+        this.p1 = new CliPlayer();
+        this.p2 = new CliPlayer();
     }
 
     public void makeTurn(final QuartoPiece piece, final int x, final int y) {
@@ -24,6 +25,42 @@ public class QuartoGame {
         
         board.placePiece(piece, x, y);
         availablePieces.remove(piece);
+    }
+
+    public Player play() {
+        Player currentPlayer = p1;
+        QuartoPiece piece = null;
+
+        piece = currentPlayer.selectPiece(availablePieces);
+        currentPlayer = (currentPlayer == p1 ? p2 : p1);
+
+        while (!winConditionMet()) {
+            // current player places the piece chose by previous player
+            // TODO: Could this be a 'validateMove()' function?
+            while (true) {
+                Move nextMove = currentPlayer.makeMove(piece, ImmutableQuartoBoard.copyOf(board));   // TODO: Do we need to pass board here? What about in player CTOR?
+                if (!board.isOccupied(nextMove.getX(), nextMove.GetY()))
+                    break;
+                else
+                    System.out.println("Cell occupied");
+            }
+
+            // current player chooses piece from available peices for next player
+            // TODO: Could this be a 'validatePiece()' function?
+            while (true) {
+                piece = currentPlayer.selectPiece(availablePieces);
+                if (availablePieces.contains(piece))
+                    break;
+                else
+                    System.out.println("Piece unavailable");
+            }
+
+            currentPlayer = (currentPlayer == p1 ? p2 : p1);
+
+            // TODO: Check if there no pieces left
+        }
+
+        return currentPlayer;
     }
 
 }
